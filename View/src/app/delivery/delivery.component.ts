4a93b1638adf97f7e8cc56ffb7f4ec2e072ae933
dluque1 <34280006+dluque1@users.noreply.github.com>
2018-01-21 01:12:02 -0500

    fix Readme

--- View/src/app/delivery/delivery.component.ts
+++ View/src/app/delivery/delivery.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero';
+//import { Hero } from '../hero';
 @Component({
   selector: 'app-delivery',
   templateUrl: './delivery.component.html',
@@ -7,10 +7,10 @@ import { Hero } from '../hero';
 })
 export class DeliveryComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  // hero: Hero = {
+  //   id: 1,
+  //   name: 'Windstorm'
+  // };
 
   constructor() { }
 
